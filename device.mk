d23e01860dd24cf83686b4a8e6cfe9d93e83c8b1
skenakun <ajansyidad2014@gmail.com>
2026-02-09 22:36:28 +0000

    munch: Initialize for Evolution X 11.x+

--- device.mk
+++ device.mk
@@ -58,5 +58,8 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Inherit Private Keys
+-include vendor/lineage-priv/keys/keys.mk
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/munch/munch-vendor.mk)
