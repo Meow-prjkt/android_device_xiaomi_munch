affccd48cff6b42e893d03a1e4c3024d13672166
skenakun <ajansyidad2014@gmail.com>
2026-05-17 09:05:42 +0000

    munch: Update blobs to munch V816.0.12.0.ULMMIXM

--- extract-files.py
+++ extract-files.py
@@ -40,7 +40,7 @@ blob_fixups: blob_fixups_user_type = {
         .binary_regex_replace(b'\x9A\x0A\x00\x94', b'\x1F\x20\x03\xD5'),
     'vendor/lib/libaudioroute_ext.so': blob_fixup()
         .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
-    'vendor/lib/hw/audio.primary.kona.so': blob_fixup()
+    'vendor/lib/hw/audio.primary.munch.so': blob_fixup()
         .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
 }  # fmt: skip
 
